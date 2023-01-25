9e6e22abbedaf628a76999a52584b1eed14a04c3
shmen-shmen <akshevchenko@proton.me>
2023-01-26 02:23:38 +0600

    Tried bunch of things. General cleanup/refactor

--- src/App.js
+++ src/App.js
@@ -14,73 +14,59 @@ function App() {
 
 const Cellz = (props) => {
 	const [showFG, setShowFG] = useState(false);
-	if (showFG) {
-		document.body.style.overflow = "hidden";
-	} else {
-		document.body.style.overflow = "auto";
-	}
+	const [inmateFg, setInmateFg] = useState({});
+	// if (showFG) {
+	// 	document.body.style.overflow = "hidden";
+	// } else {
+	// 	document.body.style.overflow = "auto";
+	// }
 	return (
 		<section className="Cellz">
-			<Cell n={"1"} showFG={showFG} setShowFG={setShowFG}></Cell>
-			<Cell n={"2"} showFG={showFG} setShowFG={setShowFG}></Cell>
-			<div className="grid-cell grid-col-span-2">
-				<h3>inmate</h3>
-			</div>
-			<div className="grid-cell">
-				<h3>inmate</h3>
-			</div>
-			<div className="grid-cell ">
-				<h3>inmate</h3>
-			</div>
+			{inmates.map((inmate) => {
+				const key = Math.random().toString().slice(2);
+				return (
+					<Cell
+						key={key}
+						inmate={inmate}
+						showFG={showFG}
+						setShowFG={setShowFG}
+						inmateFg={inmateFg}
+						setInmateFg={setInmateFg}
+					></Cell>
+				);
+			})}
 			<div className="grid-cell AMF">
 				<h1>DIGITAL PRISON</h1>
 			</div>
-			<div className="grid-cell">
-				<h3>inmate</h3>
-			</div>
-			<div className="grid-cell grid-col-span-2">
-				<h3>inmate</h3>
-			</div>
-			<Cell n={"1"} showFG={showFG} setShowFG={setShowFG}></Cell>
-			<div className="grid-cell">
-				<h3>inmate</h3>
-			</div>
-			<div className="grid-cell grid-col-span-2">
-				<h3>inmate</h3>
-			</div>
-			{showFG ? <CellZoom showFG={showFG} setShowFG={setShowFG} /> : null}
-			{showFG ? <CellZoom showFG={showFG} setShowFG={setShowFG} /> : null}
+			{showFG ? (
+				<CellZoom
+					inmate={inmateFg}
+					setInmateFg={setInmateFg}
+					showFG={showFG}
+					setShowFG={setShowFG}
+				/>
+			) : null}
 		</section>
 	);
 };
 
 const Cell = (props) => {
-	const { name, smallImg, largeImg } = inmates.inmate1;
-	const [cellSize, setCellSize] = useState("");
-	const [image, setImage] = useState(smallImg);
-	const [spanNum, setspanNum] = useState("2");
+	const { name, smallImg, aspect } = props.inmate;
+	const { showFG, setShowFG, inmateFg, setInmateFg } = props;
+	// const [largeCell, setLargeCell] = useState("");
+	// const [cellSize, setCellSize] = useState("");
+	// const [image, setImage] = useState(smallImg);
+	// const [position, setPosition] = useState("relative");
 
 	const cellHandler = (e) => {
 		// VAR-1 SMALL WINODW EXPANDS:
 		// let { width, height } = e.target.style;
-		// cellSize === "500px" ? setCellSize("") : setCellSize("500px");
+		// cellSize === "" ? setCellSize("666px") : setCellSize("");
+		// position === "relative" ? setPosition("absolute") : setPosition("relative");
 		// image === smallImg ? setImage(largeImg) : setImage(smallImg);
-		// image === smallImg ? setspanNum("10") : setspanNum("2");
-
 		// VAR-2 NEW WINODW APPEARS IN FRONT OF EVERYTHING:
-		props.setShowFG(!props.showFG);
-
-		// VAR-3 IDK WHAT THAT IS:
-		// const computedStyle = window.getComputedStyle(e.target);
-		// const copiedStyle = {};
-		// for (const prop in computedStyle) {
-		// 	if (Object.hasOwn(computedStyle, prop)) {
-		// 		const key = computedStyle[prop];
-		// 		const value = computedStyle[key];
-		// 		copiedStyle[key] = value;
-		// 	}
-		// }
-		// console.log(window.getComputedStyle(CellZoom()));
+		setShowFG(!showFG);
+		setInmateFg(props.inmate);
 	};
 
 	return (
@@ -89,10 +75,10 @@ const Cell = (props) => {
 			id={"inmate-" + name}
 			onClick={cellHandler}
 			style={{
-				width: cellSize,
-				height: cellSize,
-				backgroundImage: `url(/assets/images/${image})`,
-				gridRow: `span ${spanNum}`,
+				backgroundImage: `url(/assets/images/${smallImg})`,
+				gridColumn: `span ${aspect}`,
+				// width: `${cellSize}`,
+				// height: `${cellSize}`,
 			}}
 		>
 			<h3>{name}</h3>
@@ -102,33 +88,30 @@ const Cell = (props) => {
 
 let CellZoom = (props) => {
 	console.log("foregroundEl was rendered");
-	const { name, largeImg } = inmates.inmate1;
+	const { showFG, setShowFG, inmate, setInmateFg } = props;
+	const { name, largeImg } = inmate;
 	const hider = () => {
-		props.setShowFG(!props.showFG);
+		setShowFG(!showFG);
+		setInmateFg({});
 	};
 	return (
-		<div
-			className="cellZoom"
-			onClick={hider}
-			style={{
-				position: "absolute",
-				bottom: "calc(50% - 200px)",
-				left: "calc(50% - 250px)",
-				color: "white",
-				backgroundColor: "black",
-			}}
-		>
-			<h1>CELL ZOOM</h1>
+		<>
 			<div
+				className="cellZoom"
+				onClick={hider}
 				style={{
+					position: "absolute",
+					zIndex: "666",
 					backgroundImage: `url(/assets/images/${largeImg})`,
 					backgroundRepeat: "no-repeat",
 					backgroundSize: "cover",
-					width: "500px",
-					height: "500px",
+					width: "100%",
+					height: "100%",
 				}}
-			></div>
-		</div>
+			>
+				<h1>CELL ZOOM</h1>
+			</div>
+		</>
 	);
 };
 
--- src/inmates.js
+++ src/inmates.js
@@ -1,44 +1,52 @@
-const inmates = {
-	inmate1: {
+const inmates = [
+	{
 		name: "boxer",
 		smallImg: "boxer-col-16-dith-0-size-10.jpg",
 		largeImg: "boxer-col-64-dith-0-size-50.jpg",
+		aspect: "1",
 	},
-	inmate3: {
+	{
 		name: "fucker",
 		smallImg: "boxer-col-16-dith-0-size-10.jpg",
 		largeImg: "boxer-col-64-dith-0-size-50.jpg",
+		aspect: "2",
 	},
-	inmate4: {
+	{
 		name: "fucker",
 		smallImg: "boxer-col-16-dith-0-size-10.jpg",
 		largeImg: "boxer-col-64-dith-0-size-50.jpg",
+		aspect: "1",
 	},
-	inmate5: {
+	{
 		name: "fucker",
 		smallImg: "boxer-col-16-dith-0-size-10.jpg",
 		largeImg: "boxer-col-64-dith-0-size-50.jpg",
+		aspect: "1",
 	},
-	inmate6: {
+	{
 		name: "fucker",
 		smallImg: "boxer-col-16-dith-0-size-10.jpg",
 		largeImg: "boxer-col-64-dith-0-size-50.jpg",
+		aspect: "1",
 	},
-	inmate7: {
+	{
 		name: "fucker",
 		smallImg: "boxer-col-16-dith-0-size-10.jpg",
 		largeImg: "boxer-col-64-dith-0-size-50.jpg",
+		aspect: "1",
 	},
-	inmate8: {
+	{
 		name: "fucker",
 		smallImg: "boxer-col-16-dith-0-size-10.jpg",
 		largeImg: "boxer-col-64-dith-0-size-50.jpg",
+		aspect: "1",
 	},
-	inmate9: {
+	{
 		name: "fucker",
 		smallImg: "boxer-col-16-dith-0-size-10.jpg",
 		largeImg: "boxer-col-64-dith-0-size-50.jpg",
+		aspect: "2",
 	},
-};
+];
 
 export default inmates;
